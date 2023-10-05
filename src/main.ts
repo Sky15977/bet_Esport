e7ca74edbe3ba2866b6f78d6bb6fd10ff9d25183
Matthew <matthew@heyliot.com>
2023-10-05 16:46:06 +0200

    fix: front of user login and signup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {NestExpressApplication} from "@nestjs/platform-express";
+import { join } from  'path';
+import * as process from "process";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const path = process.cwd();
+  const views = join(path, "views");
+
+  app.setBaseViewsDir(views);
+  app.useStaticAssets(join(path, "public"));
+  app.setViewEngine("ejs");
   await app.listen(3000);
 }
 bootstrap();
